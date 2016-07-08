96a2bccf232d88dff76439c932d0b1ec556c9563
inoc603 <inoc603@gmail.com>
2016-07-08 18:35:33 +0800

    add extract and ip util

--- shell/common.sh
+++ shell/common.sh
@@ -26,3 +26,46 @@ function freeport () {
 function checkport () {
   lsof -n -i:$1 | grep LISTEN
 }
+
+# Extract compressed file
+function extract()      # Handy Extract Program
+{
+	if [ -f $1  ] ; then
+		case $1 in
+		*.tar.bz2)   tar xvjf $1     ;;
+		*.tar.gz)    tar xvzf $1     ;;
+		*.bz2)       bunzip2 $1      ;;
+		*.rar)       unrar x $1      ;;
+		*.gz)        gunzip $1       ;;
+		*.tar)       tar xvf $1      ;;
+		*.tbz2)      tar xvjf $1     ;;
+		*.tgz)       tar xvzf $1     ;;
+		*.zip)       unzip $1        ;;
+		*.Z)         uncompress $1   ;;
+		*.7z)        7z x $1         ;;
+		*)           echo "'$1' cannot be extracted via >extract<" ;;
+		esac
+	else
+		echo "'$1' is not a valid file!"
+	fi
+
+}
+
+# Get IP adress on ethernet.
+function my_ip()
+{
+	case `uname` in
+		Darwin)	interface='en0'		;;
+		*)	interface='eth0'	;;
+	esac
+
+	case $1 in
+		4)	filter='$1=="inet"'	;;
+		6)	filter='$1=="inet6"'	;;
+		*)	filter='/inet/'		;;
+
+	esac
+	MY_IP=$(ifconfig ${interface} | awk "${filter} { print \$2  }" |
+		sed -e s/addr://)
+	echo ${MY_IP:-"Not connected"}
+}
